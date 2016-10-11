777a583432df7e419cfc3fdc7c82fa7f6c5782e0
Paulo Henrique da Silva <paulo.silva@benner.com.br>
2016-10-11 08:08:59 -0300

    priorize projects with roslyn reports

--- RemoveSonarOutputDuplicates/Program.cs
+++ RemoveSonarOutputDuplicates/Program.cs
@@ -9,6 +9,13 @@ using System.Xml.Linq;
 
 namespace RemoveSonarOutputDuplicates
 {
+    public class ProjectInfo
+    {
+        public string File { get; set; }
+        public string Guid { get; set; }
+        public bool HasRoslynReport { get; set; }
+    }
+
     public class Program
     {
         static int Main(string[] args)
@@ -27,6 +34,7 @@ namespace RemoveSonarOutputDuplicates
             }
 
             Dictionary<string, string> projects = new Dictionary<string, string>();
+            List<ProjectInfo> projectsAll = new List<ProjectInfo>();
 
             foreach (var projectDir in Directory.GetDirectories(baseDir))
             {
@@ -49,6 +57,12 @@ namespace RemoveSonarOutputDuplicates
                     continue;
                 }
 
+                /*
+                xdoc.Elements()
+                    .First((xe) => xe.Name.LocalName == "ProjectInfo").Elements()
+                    .First(xe => xe.Name.LocalName == "IsExcluded").Value = "false";
+                xdoc.Save(projectInfoFileName);
+                */
 
                 string projectGuid = string.Empty;
                 try
@@ -56,7 +70,7 @@ namespace RemoveSonarOutputDuplicates
                     projectGuid = xdoc.Elements()
                         .First((xe) => xe.Name.LocalName == "ProjectInfo").Elements()
                         .First(xe => xe.Name.LocalName == "ProjectGuid").Value;
-                    Console.WriteLine("GUID {0}.", projectGuid);
+                    Console.WriteLine("GUID {0}", projectGuid);
                 }
                 catch (Exception e)
                 {
@@ -64,29 +78,72 @@ namespace RemoveSonarOutputDuplicates
                     continue;
                 }
 
-                if (projects.ContainsKey(projectGuid))
+                bool hasRoslynReportFilePath = false;
+                try
                 {
-                    try
-                    {
-                        xdoc.Elements()
-                            .First((xe) => xe.Name.LocalName == "ProjectInfo").Elements()
-                            .First(xe => xe.Name.LocalName == "IsExcluded").Value = "true";
-                        xdoc.Save(projectInfoFileName);
-                        Console.WriteLine("Duplicated project removed from analysis!");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("*** Can't set {0} excluded. Message: {1}", projectInfoFileName, e.ToString());
-                        return 1;
-                    }
+                    hasRoslynReportFilePath = xdoc.Elements()
+                        .First((xe) => xe.Name.LocalName == "ProjectInfo").Elements()
+                        .First(xe => xe.Name.LocalName == "AnalysisSettings").Elements()
+                        .Any((xe) => xe.Attributes().Any((xa) => xa.Name.LocalName == "Name" && xa.Value == "sonar.cs.roslyn.reportFilePath"));
+                    Console.WriteLine("Roslyn Report File Path: {0}", hasRoslynReportFilePath);
                 }
-                else
+                catch (Exception e)
                 {
-                    Console.WriteLine("Nothing to do.");
-                    projects[projectGuid] = projectInfoFileName;
+                    Console.WriteLine("*** Can't load project sonar.cs.roslyn.reportFilePath attribute. Message: {0}", projectInfoFileName, e.ToString());
+                    continue;
                 }
 
+                projectsAll.Add(new ProjectInfo() { File = projectInfoFileName, Guid = projectGuid, HasRoslynReport = hasRoslynReportFilePath });
+               
                 Console.WriteLine();
+            }
+
+            Console.WriteLine("Verifying duplicated projects...");
+            foreach (string guid in projectsAll.Select((pi) => pi.Guid).Distinct())
+            {
+                var duplicatedProjs = projectsAll.Where((pi) => pi.Guid == guid).ToList();
+                
+                if (duplicatedProjs.Count > 1)
+                {
+                    Console.WriteLine("There are {0} projects with GUID {1}.", duplicatedProjs.Count, guid);
+
+                    var projWithRoslyn = duplicatedProjs.FirstOrDefault((pi) => pi.HasRoslynReport);
+
+                    bool thereIsAWinner = false;
+                    for (int i = 0; i < duplicatedProjs.Count; i++)
+                    {
+                        if (projWithRoslyn != null && duplicatedProjs[i].File == projWithRoslyn.File)
+                        {
+                            Console.WriteLine("Project {0} with Roslyn report will be preserved!", duplicatedProjs[i].File);
+                            thereIsAWinner = true;
+                            continue;
+                        }
+
+                        if (i == (duplicatedProjs.Count - 1) && !thereIsAWinner)
+                        {
+                            Console.WriteLine("Remaining project {0} will be preserved!", duplicatedProjs[i].File);
+                            continue;
+                        }
+                        else
+                        {
+                            try
+                            {
+                                var xdoc = XDocument.Load(duplicatedProjs[i].File);
+                                xdoc.Elements()
+                                    .First((xe) => xe.Name.LocalName == "ProjectInfo").Elements()
+                                    .First(xe => xe.Name.LocalName == "IsExcluded").Value = "true";
+                                xdoc.Save(duplicatedProjs[i].File);
+                                Console.WriteLine("Duplicated project {0} removed from analysis!", duplicatedProjs[i].File);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("*** Can't set {0} excluded. Message: {1}", duplicatedProjs[i].File, e.ToString());
+                                return 1;
+                            }
+                        }
+                    }
+                    Console.WriteLine();
+                }
             }
 
             return 0;
